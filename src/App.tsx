eed4838f777d1668b92a04fbee95d672dd8bfb31
Dennis Mubamba <dennism501@outlook.com>
2021-08-07 10:16:38 +0200

    :truck: Mode the main app page

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
 import React from 'react';
+import CashRegister from './pages/CashRegister';
 
 function App() {
   return (
-    <div >
-
-    </div>
+    <>
+      <CashRegister />
+    </>
   );
 }
 
