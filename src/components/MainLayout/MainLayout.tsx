8583163245eab4b415258518511d79d8fe40c9e4
Dennis Mubamba <dennism501@outlook.com>
2021-08-10 20:41:01 +0200

    :truck: Created Main layout component

---
+++ src/components/MainLayout/MainLayout.tsx
@@ -0,0 +1,23 @@
+import styled from 'styled-components'
+
+export const MainContainer = styled.div`
+    display: flex;
+    height: 60vh;
+    width: 1000px;
+    font-family: monospace;
+    flex-direction: row;
+    align-items: flex-end;
+    justify-content: space-around;
+    background: #E8EDF4;
+    border-radius: 5px;
+`;
+
+export const KeypadContainer = styled.div`
+    height: 100%;
+    width: 60%;
+`;
+
+export const ChargeListContainer = styled.div`
+    height: 100%;
+    width: 40%;
+`
